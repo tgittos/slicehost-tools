9efa22e4920f37b4198357207e1bf4db78d2a141
Tim <tim@chromasplosion.com>
2011-02-10 20:13:23 +0000

    Removing sensitive information

--- dns.rb
+++ dns.rb
@@ -1,9 +1,6 @@
 require 'rubygems'
 require 'active_resource'
-
-SLICE_PUBLIC_IP = "184.106.151.168"
-api = "234fc1901b2ac939f78313c659a0ee26be5306ebbb6451d617ddfe5ab443d35a"
-SITE_URL = "https://#{api}@api.slicehost.com/"
+require 'config'
 
 class Slice < ActiveResource::Base
   self.site = SITE_URL
