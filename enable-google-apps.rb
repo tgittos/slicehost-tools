df82e8ee2aa6fd072e10e18f4caa50e031c47935
Tim <tim@chromasplosion.com>
2011-02-24 16:18:27 +0000

    Fixed borked Google Apps MX records

--- enable-google-apps.rb
+++ enable-google-apps.rb
@@ -9,18 +9,18 @@ puts "Could not find DNS for #{ARGV[0]}" and exit if zone.nil?
 
 #Create Google Apps records for that zone
 mx_values = [
-  '10 ASPMX.L.GOOGLE.COM.', 
-  '20 ALT1.ASPMX.L.GOOGLE.COM.',
-  '20 ALT2.ASPMX.L.GOOGLE.COM.',
-  '30 ASPMX2.GOOGLEMAIL.COM.',
-  '30 ASPMX3.GOOGLEMAIL.COM.',
-  '30 ASPMX4.GOOGLEMAIL.COM.',
-  '30 ASPMX4.GOOGLEMAIL.COM.',
-  '30 ASPMX5.GOOGLEMAIL.COM.'
+  { :aux => '10', :data => 'ASPMX.L.GOOGLE.COM.' }, 
+  { :aux => '20', :data => 'ALT1.ASPMX.L.GOOGLE.COM.' },
+  { :aux => '20', :data => 'ALT2.ASPMX.L.GOOGLE.COM.' },
+  { :aux => '30', :data => 'ASPMX2.GOOGLEMAIL.COM.' },
+  { :aux => '30', :data => 'ASPMX3.GOOGLEMAIL.COM.' },
+  { :aux => '30', :data => 'ASPMX4.GOOGLEMAIL.COM.' },
+  { :aux => '30', :data => 'ASPMX4.GOOGLEMAIL.COM.' },
+  { :aux => '30', :data => 'ASPMX5.GOOGLEMAIL.COM.' }
 ]
 
 mx_values.each do |mx|
-  record = Record.new(:record_type => 'MX', :zone_id => zone.id, :name => 'mail', :data => mx)
+  record = Record.new(:record_type => 'MX', :zone_id => zone.id, :name => 'mail', :data => mx[:data], :aux => mx[:aux])
   record.save 
 end
 
